5e6810dd70f63513b862ce1da22b9cc01a0bf30a
S00125622 <S00125622@mail.itsligo.ie>
2017-01-15 19:00:08 +0000

    KmeCommit

---
+++ GameClient/TestDbContext.cs
@@ -0,0 +1,49 @@
+﻿using GameData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClient
+{
+        class TestDbContext : IDisposable
+        {
+            private bool disposed = false;
+            public List<PlayerData> ScoreBoard = new List<PlayerData>();
+
+            public TestDbContext()
+            {
+                //ScoreBoard = File.ReadAllLines(@"Content\random Names with scores.csv")
+                //                             //.Skip(1) // Only needed if the first row contains the Field names
+                //                             .Select(v => PlayerData.FromCsv(v))
+                //                             .OrderByDescending(s => s.topscore)
+                //                             .ToList();
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+
+                if (!disposed)
+                {
+                    if (disposing)
+                    {
+                        // Manual release of managed resources.
+                    }
+                    // Release unmanaged resources.
+                    disposed = true;
+                }
+            }
+
+            public List<PlayerData> getTop(int count)
+            {
+                return ScoreBoard.Take(count).ToList();
+            }
+        }
+}
