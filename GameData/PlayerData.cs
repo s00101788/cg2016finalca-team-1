a5e262a9273dd2b05b390d7b7e275cbf134ecbb2
S00125622 <S00125622@mail.itsligo.ie>
2017-01-15 18:41:17 +0000

    PlayerData

--- GameData/PlayerData.cs
+++ GameData/PlayerData.cs
@@ -13,5 +13,25 @@ namespace GameData
         public string SecondName { get; set; }
         public string Tag { get; set; }
         public int XP { get; set; }
-    }
+        public Guid playerid;
+        public string GamerTag;
+        public int topscore;
+
+        public string GamerTagScore { get { return GamerTag + " ==> " + topscore.ToString(); } }
+
+        public PlayerData()
+        {
+        }
+        public static PlayerData FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            PlayerData player = new PlayerData();
+            player.playerid = Guid.NewGuid();
+            player.FirstName = values[0];
+            player.SecondName = values[1];
+            player.GamerTag = values[2];
+            player.topscore = Int32.Parse(values[3]);
+
+            return player;
+        }
 }
